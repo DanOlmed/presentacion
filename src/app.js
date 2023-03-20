240e0f13fea211d62dac47abc4df8a130b76c732
Your Name <you@example.com>
2023-03-20 20:25:54 -0300

    aplicacion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo");
+const express = require('express')
+
+const app = express();
+
+const PORT = 3001;
+
+app.get('/api', (req, res)=>{
+    res.json({mensaje: 'Hola mundo'}).status(200)
+
+});
+console.log("mitad de proceso");
+
+app.listen(PORT, ()=>{console.log('estoy escuchando en el puerto: '+PORT)})
